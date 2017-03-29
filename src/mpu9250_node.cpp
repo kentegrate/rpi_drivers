0367f05a7398c21311548a9f7024d44b3bbbf90b
Ken Takaki <kenkiki94305@gmail.com>
2017-03-30 08:20:39 +0900

    changed mpu9250 msg to sensor_msgs

--- src/mpu9250_node.cpp
+++ src/mpu9250_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
-#include <rcopter_msgs/Imu.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
 
 #ifdef HAS_BCM2835
   #include <rpi_drivers/mpu9250.hpp>
@@ -9,8 +10,8 @@
   MockMPU9250 mpu;  
 #endif
 
-rcopter_msgs::Imu createImuMsg(float* accel, float* gyro, float* mag){
-  rcopter_msgs::Imu msg;
+sensor_msgs::Imu createImuMsg(float* accel, float* gyro){
+  sensor_msgs::Imu msg;
   msg.linear_acceleration.x = accel[0];
   msg.linear_acceleration.y = accel[1];
   msg.linear_acceleration.z = accel[2];
@@ -19,6 +20,11 @@ rcopter_msgs::Imu createImuMsg(float* accel, float* gyro, float* mag){
   msg.angular_velocity.y = gyro[1];
   msg.angular_velocity.z = gyro[2];
 
+  return msg;
+}
+
+sensor_msgs::MagneticField createMagMsg(float *mag){
+  sensor_msgs::MagneticField msg;
   msg.magnetic_field.x = mag[0];
   msg.magnetic_field.y = mag[1];
   msg.magnetic_field.z = mag[2];
@@ -34,14 +40,17 @@ int main(int argc, char* argv[]){
   float gyro[3];
   float mag[3];
 
-  ros::Publisher pub = nh.advertise<rcopter_msgs::Imu>("imu", 10);
+  ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("data_raw", 10);
+  ros::Publisher mag_pub = nh.advertise<sensor_msgs::MagneticField>("mag", 10);
   ros::Rate rate(100);
 
   while(ros::ok()){
     mpu.getMotion9(accel, accel + 1, accel + 2,
 		   gyro, gyro + 1, gyro + 2,
 		   mag, mag + 1, mag + 2);
-    pub.publish(createImuMsg(accel, gyro, mag));
+    imu_pub.publish(createImuMsg(accel, gyro));
+    mag_pub.publish(createMagMsg(mag));
+    
     rate.sleep();
   }
   return 0;
