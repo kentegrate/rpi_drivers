167ad54450d88fea05473c9242f0e3ac0a7caff8
Ken Takaki <kenkiki94305@gmail.com>
2017-03-30 22:32:51 +0900

    removed offset calc from mpu9250 spi node

--- src/mpu9250_node.cpp
+++ src/mpu9250_node.cpp
@@ -40,44 +40,15 @@ int main(int argc, char* argv[]){
   float gyro[3];
   float mag[3];
 
-  float accel_offset[3];
-  float gyro_offset[3];
-  float mag_offset[3];
-  for(int i = 0; i < 3; i++){
-    accel_offset[i] = 0;
-    gyro_offset[3] = 0;
-    mag_offset[3] = 0;
-  }
 
   ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("data_raw", 10);
   ros::Publisher mag_pub = nh.advertise<sensor_msgs::MagneticField>("mag", 10);
   ros::Rate rate(100);
   std::cout << mpu.testConnection()<<std::endl;
-  for(int i = 0; i < 500; i++){
-        mpu.getMotion9(accel, accel + 1, accel + 2,
-		   gyro, gyro + 1, gyro + 2,
-		   mag, mag + 1, mag + 2);
-        rate.sleep();
-        for(int j = 0; j < 3; j++){
-          accel_offset[j] += accel[j];
-          gyro_offset[j] += gyro[j];
-          mag_offset[j] += mag[j];
-        }
-  }
-  for(int i = 0; i < 3; i++){
-    accel_offset[j] /= 500;
-    gyro_offset[j] /= 500;
-    mag_offset[j] /= 500;
-  }
   while(ros::ok()){
     mpu.getMotion9(accel, accel + 1, accel + 2,
 		   gyro, gyro + 1, gyro + 2,
 		   mag, mag + 1, mag + 2);
-    for(int i = 0; i < 3; i++){
-      accel -= accel_offset[i];
-      gyro -= gyro_offset[i];
-      mag -= mag_offset[i];
-    }
     imu_pub.publish(createImuMsg(accel, gyro));
     mag_pub.publish(createMagMsg(mag));
     
